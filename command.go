b63095092918b67bd944fb3aa8fd07f192fe50b4
7minutech <elijohnson1107@gmail.com>
2025-10-12 16:39:14 -0500

    feat: handlerRegister() & handlerLogin() checks if user exists before logging in

--- command.go
+++ command.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"fmt"
+	"time"
 
-	"github.com/7minutech/gator_go/internal/config"
+	"github.com/7minutech/gator_go/internal/database"
+	"github.com/google/uuid"
 )
-
-type state struct {
-	Config *config.Config
-}
 
 type command struct {
 	name string
@@ -35,12 +36,60 @@ func (c *commands) register(name string, f func(*state, command) error) {
 }
 
 func handlerLogin(s *state, cmd command) error {
-	if len(cmd.args) == 0 {
+	if len(cmd.args) == 0 || len(cmd.args) > 1 {
 		return fmt.Errorf("error: login expects a single argument, the username")
 	}
-	if err := s.Config.SetUser(cmd.args[0]); err != nil {
-		return err
+
+	name := cmd.args[0]
+
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return fmt.Errorf("error: %s does not exist", name)
+		}
+		return fmt.Errorf("error: fetching %s from users: %w", name, err)
 	}
+
+	if err := s.Config.SetUser(name); err != nil {
+		return fmt.Errorf("error: setting login for user %s: %w", name, err)
+	}
+
 	fmt.Printf("user %s has been set\n", s.Config.CurrentUserName)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 || len(cmd.args) > 1 {
+		return fmt.Errorf("error: register expects a single argument, the name")
+	}
+
+	name := cmd.args[0]
+
+	_, err := s.db.GetUser(context.Background(), name)
+	if err == nil {
+		return fmt.Errorf("error: %s is already registered", name)
+	}
+	if !errors.Is(err, sql.ErrNoRows) {
+		return fmt.Errorf("error: checking if name: %s exists in users: %w", name, err)
+	}
+
+	userParams := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      name,
+	}
+	newUser, err := s.db.CreateUser(context.Background(), userParams)
+	if err != nil {
+		return fmt.Errorf("error: creating user %s: %w", name, err)
+	}
+
+	if err := s.Config.SetUser(newUser.Name); err != nil {
+		return fmt.Errorf("error: registering user %s: %w", name, err)
+	}
+
+	fmt.Printf("user %s has been created\n", newUser.Name)
+	fmt.Printf("%+v\n", newUser)
+
 	return nil
 }
