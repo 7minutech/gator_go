8291d014f1cdd1779fdea2e76f69620a66f2de8d
7minutech <elijohnson1107@gmail.com>
2025-10-17 16:51:26 -0500

    feat: migration create feed_follows table

---
+++ sql/schema/003_feed_follow.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE feed_follows (
+    id uuid PRIMARY KEY,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone NOT NULL,
+    user_id references users(id) ON DELETE CASCADE NOT NULL,
+    feed_id references feeds(id) ON DELETE CASCADE NOT NULL,
+    UNIQUE (user_id, feed_id)
+);
+
+-- +goose Down
+DROP TABLE feed_follows;
