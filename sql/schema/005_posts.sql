f7e2204c4bc5d16b84489ecfb506d8b5df73dbbc
7minutech <elijohnson1107@gmail.com>
2025-10-18 16:12:58 -0500

    fix: posts published_at should be string b/c RSSItem.PubDate is string

--- sql/schema/005_posts.sql
+++ sql/schema/005_posts.sql
@@ -6,7 +6,7 @@ CREATE TABLE posts (
     title VARCHAR(255) NOT NULL,
     url VARCHAR(255) NOT NULL UNIQUE,
     description Text NOT NULL,
-    published_at timestamp with time zone NOT NULL,
+    published_at VARCHAR(255) NOT NULL,
     feed_id uuid references feeds(id) ON DELETE CASCADE NOT NULL
 );
 
