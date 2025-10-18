886e205f408e385b47038a83ef3b3759e6ca30a1
7minutech <elijohnson1107@gmail.com>
2025-10-18 16:09:54 -0500

    fix: post descriptions & published_at should be not null

--- sql/schema/005_posts.sql
+++ sql/schema/005_posts.sql
@@ -5,8 +5,8 @@ CREATE TABLE posts (
     updated_at timestamp with time zone NOT NULL,
     title VARCHAR(255) NOT NULL,
     url VARCHAR(255) NOT NULL UNIQUE,
-    description Text,
-    published_at timestamp with time zone,
+    description Text NOT NULL,
+    published_at timestamp with time zone NOT NULL,
     feed_id uuid references feeds(id) ON DELETE CASCADE NOT NULL
 );
 
