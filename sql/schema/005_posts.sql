e00a10f8a21867de3c2bb70202ccec9f16774d76
7minutech <elijohnson1107@gmail.com>
2025-10-18 16:03:31 -0500

    fix: url field should be unique for post

--- sql/schema/005_posts.sql
+++ sql/schema/005_posts.sql
@@ -4,7 +4,7 @@ CREATE TABLE posts (
     created_at timestamp with time zone NOT NULL,
     updated_at timestamp with time zone NOT NULL,
     title VARCHAR(255) NOT NULL,
-    url VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL UNIQUE,
     description Text,
     published_at timestamp with time zone,
     feed_id uuid references feeds(id) ON DELETE CASCADE NOT NULL
