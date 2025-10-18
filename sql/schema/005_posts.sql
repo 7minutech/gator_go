bfe7c1622d3381b7bae9c5edf4c2d5764119b891
7minutech <elijohnson1107@gmail.com>
2025-10-18 15:44:26 -0500

    feat: posts table migration

---
+++ sql/schema/005_posts.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE posts (
+    id uuid PRIMARY KEY,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    description Text,
+    published_at timestamp with time zone,
+    feed_id uuid references feeds(id) ON DELETE CASCADE NOT NULL
+);
+
+-- +goose Down
+DROP TABLE posts;
